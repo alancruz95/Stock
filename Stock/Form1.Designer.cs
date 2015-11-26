638bca37c0cc5f4bbafdca3182558b100fe21405
Alan Cruz <alancruz95@hotmail.com>
2015-11-26 10:59:54 -0600

    Primer commit

---
+++ Stock/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Stock
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcularCodBarras = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalcularCodBarras
+            // 
+            this.btnCalcularCodBarras.Location = new System.Drawing.Point(42, 43);
+            this.btnCalcularCodBarras.Name = "btnCalcularCodBarras";
+            this.btnCalcularCodBarras.Size = new System.Drawing.Size(126, 55);
+            this.btnCalcularCodBarras.TabIndex = 0;
+            this.btnCalcularCodBarras.Text = "Calcular Codigo de Barras";
+            this.btnCalcularCodBarras.UseVisualStyleBackColor = true;
+            this.btnCalcularCodBarras.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 532);
+            this.Controls.Add(this.btnCalcularCodBarras);
+            this.Name = "frmPrincipal";
+            this.Text = "Inventario";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcularCodBarras;
+
+
+
+    }
+}
+
---
+++ Stock/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string CalcularCodBarras()
+        {
+            string fecha;
+            fecha = DateTime.Today.Year.ToString() + DateTime.Today.Month.ToString();
+                
+                if(Convert.ToInt32(DateTime.Today.Day.ToString())<10)
+                      fecha = fecha + "0" + DateTime.Today.Day.ToString();
+                else
+                    fecha = fecha + DateTime.Today.Day.ToString();
+
+            MessageBox.Show(fecha);
+            return null;
+
+        }
+        private void carToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string CodBarras = CalcularCodBarras();
+        }
+    }
+}
